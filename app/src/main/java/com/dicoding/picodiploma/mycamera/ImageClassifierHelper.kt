8fec35cc3b92f33bc40163a6daab5f1750f8d0cc
ridzky rahim <ridzkyrhm37@gmail.com>
2024-04-16 09:50:06 +0700

    starter project

---
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/ImageClassifierHelper.kt
@@ -0,0 +1,143 @@
+package com.dicoding.picodiploma.mycamera
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.Build
+import android.os.SystemClock
+import android.util.Log
+import android.view.Surface
+import androidx.camera.core.ImageProxy
+import com.google.android.gms.tflite.client.TfLiteInitializationOptions
+import com.google.android.gms.tflite.gpu.support.TfLiteGpu
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.gpu.CompatibilityList
+import org.tensorflow.lite.support.common.ops.CastOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.core.vision.ImageProcessingOptions
+import org.tensorflow.lite.task.gms.vision.TfLiteVision
+import org.tensorflow.lite.task.gms.vision.classifier.Classifications
+import org.tensorflow.lite.task.gms.vision.classifier.ImageClassifier
+
+class ImageClassifierHelper(
+    private var threshold: Float = 0.1f,
+    private var maxResults: Int = 3,
+    private val modelName: String = "mobilenet_v1.tflite", // penulisan value harus benar besar kecilnya
+    val context: Context,
+    val classifierListener: ClassifierListener?
+) {
+    private var imageClassifier: ImageClassifier? = null
+
+    //memeriksa ketersediaan GPU Delegate dan menginisialisasi TFLiteVision
+    init {
+        TfLiteGpu.isGpuDelegateAvailable(context).onSuccessTask { gpuAvailable ->
+            val optionBuilder = TfLiteInitializationOptions.builder()
+            if (gpuAvailable) {
+                optionBuilder.setEnableGpuDelegateSupport(true)
+            }
+            TfLiteVision.initialize(context, optionBuilder.build())
+        }.addOnSuccessListener {
+            setupImageClassifier()
+        }.addOnFailureListener {
+            classifierListener?.onError(context.getString(R.string.tflitevision_is_not_initialized_yet))
+        }
+    }
+
+    private fun setupImageClassifier() {
+        val optionsBuilder = ImageClassifier.ImageClassifierOptions.builder()
+            .setScoreThreshold(threshold)
+            .setMaxResults(maxResults)
+        val baseOptionsBuilder = BaseOptions.builder()
+
+        if (CompatibilityList().isDelegateSupportedOnThisDevice) {
+            baseOptionsBuilder.useGpu()
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+            baseOptionsBuilder.useNnapi()
+        } else {
+            // Menggunakan CPU
+            baseOptionsBuilder.setNumThreads(4)
+        }
+
+        optionsBuilder.setBaseOptions(baseOptionsBuilder.build())
+
+        try {
+            imageClassifier = ImageClassifier.createFromFileAndOptions(
+                context,
+                modelName,
+                optionsBuilder.build()
+            )
+        } catch (e: IllegalStateException) {
+            classifierListener?.onError(context.getString(R.string.image_classifier_failed))
+            Log.e(TAG, e.message.toString())
+        }
+    }
+
+    fun classifyImage(image: ImageProxy) {
+
+        // memeriksa TfLiteVision sudah diinisialisasi atau belum
+        if (!TfLiteVision.isInitialized()) {
+            val errorMessage = context.getString(R.string.tflitevision_is_not_initialized_yet)
+            Log.e(TAG, errorMessage)
+            classifierListener?.onError(errorMessage)
+            return
+        }
+
+        if (imageClassifier == null) {
+            setupImageClassifier()
+        }
+
+        val imageProcessor = ImageProcessor.Builder()
+            .add(ResizeOp(224, 224, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+            .add(CastOp(DataType.UINT8))
+            .build()
+
+        val tensorImage =
+            imageProcessor.process(TensorImage.fromBitmap(toBitmap(image))) // mengubah bitmap ke tensorImage
+
+        val imageProcessingOptions = ImageProcessingOptions.builder()
+            .setOrientation(getOrientationFromRotation(image.imageInfo.rotationDegrees))
+            .build()
+
+        //inference Time
+        var inferenceTime = SystemClock.uptimeMillis()
+        val results = imageClassifier?.classify(tensorImage, imageProcessingOptions)
+        inferenceTime = SystemClock.uptimeMillis() - inferenceTime
+        classifierListener?.onResults(
+            results,
+            inferenceTime
+        )
+    }
+
+    //mengubah imageProxy ke Bitmap
+    private fun toBitmap(image: ImageProxy): Bitmap {
+        val bitmapBuffer = Bitmap.createBitmap(
+            image.width,
+            image.height,
+            Bitmap.Config.ARGB_8888
+        )
+        image.use { bitmapBuffer.copyPixelsFromBuffer(image.planes[0].buffer) }
+        image.close()
+        return bitmapBuffer
+    }
+
+    private fun getOrientationFromRotation(rotation: Int): ImageProcessingOptions.Orientation {
+        return when (rotation) {
+            Surface.ROTATION_270 -> ImageProcessingOptions.Orientation.BOTTOM_RIGHT
+            Surface.ROTATION_180 -> ImageProcessingOptions.Orientation.RIGHT_BOTTOM
+            Surface.ROTATION_90 -> ImageProcessingOptions.Orientation.TOP_LEFT
+            else -> ImageProcessingOptions.Orientation.RIGHT_TOP
+        }
+    }
+
+    interface ClassifierListener {
+        fun onError(error: String)
+        fun onResults(result: List<Classifications>?, inferenceTime: Long)
+    }
+
+    companion object {
+        private const val TAG = "imageClassifierHelper"
+    }
+
+}
